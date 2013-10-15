a747cf28265ecfedd599ef880fba544ca73a60e1
alejandro <alex@losmaquiladores.com>
2013-10-15 14:44:33 -0500

    borra images

--- diario/js/FacebookSDK/channel.php
+++
@@ -1,6 +0,0 @@
-<?php
-	header("Pragma: public");
-	header("Cache-Control: maxage=31536000");
-	header('Expires: '.gmdate('D, d M Y H:i:s', time()+31536000).' GMT'); ?>
-
-<script src="https://connect.facebook.net/es_ES/all.js"></script>
